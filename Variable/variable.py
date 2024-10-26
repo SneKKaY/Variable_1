8fbc07c76035e49280f2804b8b5ca20eb9b09e89
SneKKaY <120265382+SneKKaY@users.noreply.github.com>
2024-10-26 13:10:50 +0300

    Add files via upload

---
+++ Variable/variable.py
@@ -0,0 +1,7 @@
+
+done_homework = 12
+spent_timeOfHour = 1.5
+nameOfCurse = 'Python'
+timeForOneTask = spent_timeOfHour / done_homework
+print("Курс:", nameOfCurse, " всего задач:", done_homework, " всего часов:", spent_timeOfHour,
+      " среднее время выполнения:", timeForOneTask  )
